65ce85dc167436d948b457e60f97ab0e2db2846b
JVelcich2313 <jadevelcich@gmail.com>
2026-01-29 14:29:20 -0600

    changed to color list

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-# ###This code will not work in repl.it as there is no access to the colorgram package here.###
-# ##We talk about this in the video tutorials##
-# import colorgram
-
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     rgb_colors.append(color.rgb)
-
-# print(rgb_colors)
-
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-toby = Turtle()
+# import colorgram
+
+toby = t.Turtle()
+t.colormode(255)
+toby.speed("fastest")
+toby.pensize(15)
+color_list =[(200, 10, 35), (247, 236, 37), (239, 231, 3), (36, 216, 77), (223, 159, 61), (39, 79, 185), (28, 39, 159), (210, 73, 16), (17, 151, 18), (239, 39, 152), (65, 9, 27), (188, 14, 9), (216, 25, 127), (218, 140, 198), (223, 161, 7), (59, 12, 7), (67, 202, 227), (10, 96, 60), (84, 80, 212), (17, 19, 52), (237, 157, 218), (106, 232, 195), (99, 205, 136), (212, 84, 58), (8, 222, 235), (236, 171, 161)]
+
+for y in range(-200, 251, 50):
+    for x in range(-200, 251, 50):
+        toby.setpos(x,y)
+        toby.dot(random.choice(color_list))
+        toby.pendown()
+        toby.penup()
 
 
-toby.hideturtle()
 
-colors = [
-    "hot pink",
-    "dark orange",
-    "yellow",
-    "lime",
-    "light sky blue",
-    "indigo",
-    "dark magenta",
-    "turquoise",
-]
-
-
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        toby.forward(100)
-        toby.right(angle)
-
-
-for shape_side_n in range(3, 11):
-    toby.color(random.choice(colors))
-    draw_shape(shape_side_n)
-
-
+screen = t.Screen()
+screen.exitonclick()
     
     
     
@@ -49,6 +28,3 @@ for shape_side_n in range(3, 11):
     
 
 
-
-screen = Screen()
-screen.exitonclick()
